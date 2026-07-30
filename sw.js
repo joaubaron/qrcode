ec1f3167a2c541dd1e7d2d637eccbae57c6e09d9
joaubaron <joaubaron@users.noreply.github.com>
2026-07-30 17:12:52 +0000

    deploy: 47dbb1c3d0e18c03e62a7983237aa0ac450ca3f8

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker 
 // Toda vez que trocar fotos/áudios, a versão será atualizada automaticamente pelo deploy.yml
-const CACHE_VERSION = '30.07.2026-1355';
+const CACHE_VERSION = '30.07.2026-1412';
 const CACHE_NAME = `qrcode-${CACHE_VERSION}`;
 const ASSETS = [
 
